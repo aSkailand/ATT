7ba9353111a91ca46a39218fca2472d86a188cee
trongt16 <trongt16@uia.no>
2017-04-03 15:56:02 +0200

    testing out branch gridssystem

--- Connect4/src/Runner.java
+++ Connect4/src/Runner.java
@@ -2,4 +2,7 @@
  * Created by aslak on 03.04.17.
  */
 public class Runner {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
--- Connect4/src/View.java
+++ Connect4/src/View.java
@@ -2,4 +2,5 @@
  * Created by aslak on 03.04.17.
  */
 public class View {
+
 }
