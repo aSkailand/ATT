f7601c9b2e2fab385db8e8628437bdb53a21c6ca
trongt16 <trongt16@uia.no>
2017-04-20 13:48:08 +0200

    Made single row check for all win checks

--- Connect4/src/GameBoardController.java
+++ Connect4/src/GameBoardController.java
@@ -39,7 +39,7 @@ public class GameBoardController implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
 
-        System.out.println("\nTURN: "+ gameBoardModel.getNumMove());
+        System.out.println("\nTURN: " + gameBoardModel.getNumMove());
         System.out.println("**********");
 
         // Check the number on the clicked button
@@ -59,7 +59,7 @@ public class GameBoardController implements ActionListener {
         gameBoardModel.addMove();
 
         // Check if Draw
-        if(gameBoardModel.getNumMove() == GameBoardModel.numRow*GameBoardModel.numCol) System.out.println("DRAW?");
+        if (gameBoardModel.getNumMove() == GameBoardModel.numRow * GameBoardModel.numCol) System.out.println("DRAW?");
 
         // Change option buttons' color to current player
         colorOptionButtons(gameBoardModel.getPlayerColor(gameBoardModel.getCurrentPlayer()));
@@ -76,104 +76,78 @@ public class GameBoardController implements ActionListener {
     // TODO: Move this function over to model when done here with all win - conditions
     // TODO: Consider change it from void -> GameBoardModel.player. May open more flexibility.
     // CHECK WIN CONDITIONS
+
+    // CHECK ALL
     void checkWinAllConditions(GameBoardModel.player player) {
         System.out.println("CHECK WIN: " + player);
 
         System.out.println("\tcheck Horizontal:");
-        checkWinHorizontal(player);
+        checkWinHorizontal_All(player);
 
         System.out.println("\tcheck Vertical:");
-        checkWinVertical(player);
+        checkWinVertical_All(player);
 
         System.out.println("\tcheck Descending:");
-        checkWinDescendingDiagonal(player);
+        checkWinDescendingDiagonal_All(player);
 
         System.out.println("\tcheck Ascending:");
-        checkWinAscendingDiagonal(player);
+        checkWinAscendingDiagonal_All(player);
 
         System.out.println("");
     }
 
-    void checkWinHorizontal(GameBoardModel.player player) {
+    boolean checkWinVertical_All(GameBoardModel.player player) {
+        boolean foundWin = false;
 
-        // Check through all rows
         for (int i = 0; i < GameBoardModel.numRow; i++) {
-
-            // Instantiating list
-            ArrayList<Integer> lister = new ArrayList<>();  // 1D binary list of current row
-
-            for (int j = 0; j < GameBoardModel.numCol; j++) {
-                if (gameBoardModel.getSlotOccupancy(j,i).equals(player)) lister.add(1);
-                else lister.add(0);
-            }
-
-            // Find win
-            colorWinningRow(0, i, 1, 0, lister, player);
-
+            if (checkWinVertical_Single(i, 0, player)) foundWin = true;
         }
+
+        return foundWin;
     }
 
-    void checkWinVertical(GameBoardModel.player player) {
+    boolean checkWinHorizontal_All(GameBoardModel.player player) {
 
-        // Check through all columns
-        for (int i = 0; i < GameBoardModel.numCol; i++) {
+        boolean foundWin = false;
 
-            // Instantiating list
-            ArrayList<Integer> lister = new ArrayList<>();  // 1D binary list of current row
-
-            for (int j = 0; j < GameBoardModel.numRow; j++) {
-                if (gameBoardModel.getSlotOccupancy(i,j).equals(player)) lister.add(1);
-                else lister.add(0);
-            }
-
-            // Find win
-            colorWinningRow(i, 0, 0, 1, lister, player);
-
+        for (int i = 0; i < GameBoardModel.numRow; i++) {
+            if (checkWinHorizontal_Single(0, i, player)) foundWin = true;
         }
+
+        return foundWin;
+
     }
 
-    void checkWinAscendingDiagonal(GameBoardModel.player player) {
+    boolean checkWinAscendingDiagonal_All(GameBoardModel.player player) {
 
-        // Declare early variables
+        boolean foundWin = false;
+
         int init_x = 0;
         int init_y = GameBoardModel.numRow - GameBoardModel.winInRow;
 
         int last_x = GameBoardModel.numCol - GameBoardModel.winInRow;
         int last_y = 0;
 
-        // Run until x has reached its end
         while (init_x <= last_x) {
 
-            // Reset variables
             int cur_x = init_x;
             int cur_y = init_y;
 
-            // Instantiating list
-            ArrayList<Integer> lister = new ArrayList<>();  // 1D binary list of current row
-
-            // Check if x or y hit their max limit
-            while (cur_x < GameBoardModel.numCol && cur_y < GameBoardModel.numRow) {
-
-                // Check current tile's owner
-                if (gameBoardModel.getSlotOccupancy(cur_x, cur_y).equals(player)) lister.add(1);
-                else lister.add(0);
-
-                // Increments both, simulating ascending rightwards.
-                cur_x++;
-                cur_y++;
-            }
-
-            // Find win
-            colorWinningRow(init_x, init_y, 1, 1, lister, player);
+            if (checkWinAscendingDiagonal_Single(cur_x, cur_y, player)) foundWin = true;
 
             // Moving PLAYER_1 step further
             if (init_y != last_y) init_y--;
             else init_x++;
 
         }
+
+        return foundWin;
+
     }
 
-    void checkWinDescendingDiagonal(GameBoardModel.player player) {
+    boolean checkWinDescendingDiagonal_All(GameBoardModel.player player) {
+
+        boolean foundWin = false;
 
         // Declare early variables
         int init_x = 0;
@@ -182,44 +156,139 @@ public class GameBoardController implements ActionListener {
         int last_x = GameBoardModel.numCol - GameBoardModel.winInRow;
         int last_y = GameBoardModel.numRow - 1;
 
-        // Run until x has reached its end
         while (init_x <= last_x) {
 
-            // Reset variables
             int cur_x = init_x;
             int cur_y = init_y;
 
-            // Instantiating list
-            ArrayList<Integer> lister = new ArrayList<>();  // 1D binary list of current row
-
-            // Check if x or y hit their max limit
-            while (cur_x < GameBoardModel.numCol && cur_y >= 0) {
-
-                // Check current tile's owner
-                if (gameBoardModel.getSlotOccupancy(cur_x, cur_y).equals(player)) lister.add(1);
-                else lister.add(0);
-
-                // Increments both, simulating ascending rightwards.
-                cur_x++;
-                cur_y--;
-            }
-
-            // Find win
-            colorWinningRow(init_x, init_y, 1, -1, lister, player);
+            if (checkWinDescendingDiagonal_Single(cur_x, cur_y, player)) foundWin = true;
 
             // Moving PLAYER_1 step further
             if (init_y != last_y) init_y++;
             else init_x++;
 
-
         }
+
+        return foundWin;
+
     }
+
+
+    // CHECK SINGLE
+    // todo: remove int y parameter?
+    boolean checkWinVertical_Single(int x, int y, GameBoardModel.player player) {
+
+        // Instantiating list
+        ArrayList<Integer> lister = new ArrayList<>();  // 1D binary list of current row
+        for (int j = 0; j < GameBoardModel.numRow; j++) {
+            if (gameBoardModel.getSlotOccupancy(x, j).equals(player)) lister.add(1);
+            else lister.add(0);
+        }
+
+        // Prematurely return if not enough same pieces
+        if (!(Collections.frequency(lister, 1) >= GameBoardModel.winInRow)) {
+            return false;
+        }
+
+        // Find win
+        return colorWinningRow(x, 0, 0, 1, lister, player);
+    }
+    // todo: remove int x parameter?
+    boolean checkWinHorizontal_Single(int x, int y, GameBoardModel.player player) {
+
+        // Instantiating list
+        ArrayList<Integer> lister = new ArrayList<>();  // 1D binary list of current row
+        for (int j = 0; j < GameBoardModel.numCol; j++) {
+            if (gameBoardModel.getSlotOccupancy(j, y).equals(player)) lister.add(1);
+            else lister.add(0);
+        }
+
+        // Prematurely return if not enough same pieces
+        if (!(Collections.frequency(lister, 1) >= GameBoardModel.winInRow)) {
+            return false;
+        }
+
+        // Find win
+        return colorWinningRow(0, y, 1, 0, lister, player);
+    }
+
+    boolean checkWinAscendingDiagonal_Single(int x, int y, GameBoardModel.player player) {
+
+        // Find initial values
+        while (0 < x && 0 < y) {
+            x--;
+            y--;
+        }
+
+        // Declare initial values
+        int init_x = x;
+        int init_y = y;
+
+        // Instantiating list
+        ArrayList<Integer> lister = new ArrayList<>();  // 1D binary list of current row
+
+        // Check if x or y hit their max limit
+        while (x < GameBoardModel.numCol && y < GameBoardModel.numRow) {
+
+            // Check current tile's owner
+            if (gameBoardModel.getSlotOccupancy(x, y).equals(player)) lister.add(1);
+            else lister.add(0);
+
+            // Increments both, simulating ascending rightwards.
+            x++;
+            y++;
+        }
+
+        // Prematurely return if not enough same pieces
+        if (!(Collections.frequency(lister, 1) >= GameBoardModel.winInRow)) {
+            return false;
+        }
+
+        // Find win
+        return colorWinningRow(init_x, init_y, 1, 1, lister, player);
+    }
+
+    boolean checkWinDescendingDiagonal_Single(int x, int y, GameBoardModel.player player){
+
+        // Find initial values
+        while (0 < x && y < GameBoardModel.numRow-1) {
+            x--;
+            y++;
+        }
+
+        // Declare initial values
+        int init_x = x;
+        int init_y = y;
+
+        // Instantiating list
+        ArrayList<Integer> lister = new ArrayList<>();  // 1D binary list of current row
+
+        // Check if x or y hit their max limit
+        while (x < GameBoardModel.numCol && 0 <= y) {
+
+            // Check current tile's owner
+            if (gameBoardModel.getSlotOccupancy(x, y).equals(player)) lister.add(1);
+            else lister.add(0);
+
+            // Increments both, simulating ascending rightwards.
+            x++;
+            y--;
+        }
+
+        // Prematurely return if not enough same pieces
+        if (!(Collections.frequency(lister, 1) >= GameBoardModel.winInRow)) {
+            return false;
+        }
+
+        return colorWinningRow(init_x, init_y, 1, -1, lister, player);
+
+    }
+
 
     boolean colorWinningRow(int init_x, int init_y, int increment_x, int increment_y, ArrayList<Integer> lister, GameBoardModel.player player) {
 
-
         // Prematurely return if not enough same pieces
-        if (!(Collections.frequency(lister, 1) >= GameBoardModel.winInRow)){
+        if (!(Collections.frequency(lister, 1) >= GameBoardModel.winInRow)) {
             return false;
         }
 
@@ -257,7 +326,7 @@ public class GameBoardController implements ActionListener {
 
                     // Color win
                     for (int j = 0; j < counter; j++) {
-                        gameBoardPanel.getSlot(x,y).piece.setBackground(WinColor);
+                        gameBoardPanel.getSlot(x, y).piece.setBackground(WinColor);
                         x += increment_x;
                         y += increment_y;
                     }
@@ -270,7 +339,6 @@ public class GameBoardController implements ActionListener {
 
         }
 
-        System.out.println("winInSight: "+winInSight);
         return winInSight;
 
     }
@@ -302,7 +370,7 @@ public class GameBoardController implements ActionListener {
     }
 
     // Check if given column is playable
-    boolean playableCol(int chosenCol){
+    boolean playableCol(int chosenCol) {
         return gameBoardModel.getListOccupancy().get(chosenCol).indexOf(GameBoardModel.player.PLAYER_NONE) != -1;
     }
 
